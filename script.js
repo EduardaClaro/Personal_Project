a4dc6fdd7f02cdad14f6bcd0c4ec19f25e1e6b79
EduardaClaro <mariaeduardaclaro06@gmali.com>
2025-05-09 15:43:31 -0300

    :sparkles: feat: jogo_da_velha

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navLinks = document.querySelector('header nav a');
+const currentPlayerDisplay = document.querySelector(".currentPlayer");
+let selected = [];
+let player = "X";
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+const winningCombinations = [
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [3, 6, 9], 
+    [1, 5, 9], 
+    [3, 5, 7],
+];
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add ('active');
-            })
-        }
-    })
+function init() {
+    selected = Array(10).fill(null); 
+    player = "X";
+    currentPlayerDisplay.innerHTML = `Jogador da vez: ${player}`;
+
+    document.querySelectorAll(".game button").forEach((button) => {
+        button.innerHTML = "";
+        button.disabled = false;
+        button.addEventListener("click", newMove);
+    });
 }
 
-  
+function newMove(e) {
+    const index = parseInt(e.target.getAttribute("data-i"));
+    e.target.innerHTML = player;
+    e.target.disabled = true;
+    selected[index] = player;
+
+    setTimeout(check, 100);
+
+    player = player === "X" ? "O" : "X";
+    currentPlayerDisplay.innerHTML = `Jogador da vez: ${player}`;
+}
+
+function check() {
+    const lastPlayer = player === "X" ? "O" : "X";
+
+    for (const pos of winningCombinations) {
+        if (pos.every((index) => selected[index] === lastPlayer)) {
+            alert(`O jogador '${lastPlayer}' ganhou!`);
+            init();
+            return;
+        }
+    }
+
+    if (selected.slice(1).every((item) => item !== null)) {
+        alert("Deu empate!");
+        init();
+        return;
+    }
+}
+
+init();
